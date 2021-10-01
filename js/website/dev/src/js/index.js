b515084c17d559be05998cd8312351ba4246a544
subtosharki <camywam@yahoo,com>
2021-10-01 17:12:24 -0400

    extra website

--- js/website/dev/src/js/index.js
+++ js/website/dev/src/js/index.js
@@ -8,7 +8,7 @@ const startTimestamp = new Date();
 const app = express();
 const port = process.env.PORT || '3000';
 const db = require('../database/ConnectDB.js');
-app.use('/api/discord', require('../api/discord'));
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../html/index.html'));
